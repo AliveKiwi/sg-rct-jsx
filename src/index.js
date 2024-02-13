6e55008469b147644aa3726d0048f27cd6157c41
AliveKiwi <prateekjais.dev@gmail.com>
2024-02-14 01:36:56 +0530

    4. Print name and age

--- src/index.js
+++ src/index.js
@@ -10,22 +10,14 @@ const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
 function App() {
-  // let message = 123;            //=> Prints 123
-  // let message = 'abc';          //=> Prints abc
-  // let message = true;           //=> Shows nothing
-  // let message = undefined;      //=> Shows nothing
-  // let message = null;           //=> Shows nothing
-  // let message = [1, 2, 3];      //=> Prints nothing
-  // let message = ['abc', 'def']; //=> Prints abcdef
-  // let message = {}              //=> Throws Error, Check error console log
+  const name = 'Prateek';
+  const age = 29;
 
-  const date = new Date();
-  const time = date.toLocaleTimeString();
-
-  return <h1>{time}</h1>;
-
-  // Another way to do the same thing as above
-  // return <h1>{new Date().toLocaleTimeString()}</h1>;
+  return (
+    <h1>
+      Hi, my name is {name} and my age is {age}
+    </h1>
+  );
 }
 
 // 5) Show the component on the screen
